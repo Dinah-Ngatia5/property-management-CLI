be3db597d62af5dc466fac4bf312a8a651fbab6c
Dinah-Ngatia5 <dinah.ngatia@student.moringaschool.com>
2024-05-07 03:35:51 +0300

    Made all the necessary class declaration, will maybe add more later

--- models.py
+++ models.py
@@ -1,4 +1,6 @@
 import sqlite3
+from datetime import datetime
+from contextlib import contextmanager
 
 conn = sqlite3.connect('property_management.db')
 cursor = conn.cursor()
@@ -8,6 +10,7 @@ cursor.execute("""
 CREATE TABLE IF NOT EXISTS properties (
     id INTEGER PRIMARY KEY,
     address TEXT NOT NULL,
+    city TEXT NOT NULL,
     rent_amount INTEGER NOT NULL,
     status TEXT NOT NULL,
     owner_id INTEGER,
@@ -52,24 +55,45 @@ CREATE TABLE IF NOT EXISTS maintenance_requests (
 conn.commit()
 conn.close()
 
+
+@contextmanager
+def get_cursor():
+    conn = sqlite3.connect("property_management.db")
+    try:
+        yield conn.cursor()
+    finally:
+        conn.close()
+
 class Property:
-    def __init__(self, address, rent_amount, status="Available", owner_id=None):
+    def __init__(self, address, city, rent_amount, status="Available", owner_id=None):
         self.address= address
         self.rent_amount= rent_amount
         self.status= status
         self.owner_id= owner_id
+        self.city= city
 
     def save(self):
-        conn = sqlite3.connect("property_management.db")
-        cursor= conn.cursor()
+       with get_cursor() as cursor:
         cursor.execute("""
-            INSERT INTO properties (address, rent_amount, status, owner_id)
+            INSERT INTO properties (address, city, rent_amount, status, owner_id)
             VALUES(?, ?, ?, ?)
-""", (self.address, self.rent_amount, self.status, self.owner_id))
-        conn.commit()
-        conn.close()
-
-
+""", (self.address, self.city, self.rent_amount, self.status, self.owner_id))
+       
+    
+    @staticmethod
+    def get_all():
+        with get_cursor() as cursor:
+            cursor.execute("SELECT * FROM properties")
+            return cursor.fetchall
+        
+    
+    def get_by_id(property_id):
+        with get_cursor() as cursor:
+            cursor.execute("SELECT * FROM properties WHERE id= ?", (property_id,))
+            return cursor.fetchone()
+        
+    #will add methods like delete, update, etc later
+ 
 
 class Owner:
     def __init__(self, name, email, phone):
@@ -78,13 +102,191 @@ class Owner:
         self.phone = phone
 
     def save(self):
-        conn = sqlite3.connect("property_management.db")
-        cursor= conn.cursor()
-        cursor.execute("""
-INSERT INTO owners (name, email, phone)
-                       VALUES(?, ?, ?)
-""", (self.name, self.email, self.phone))
-        conn.commit()
-        conn.close()
-
+        with get_cursor() as cursor:
+            cursor.execute("INSERT INTO owners (name, email, phone) VALUES(?, ?, ?)",
+                            (self.name, self.email, self.phone))
        
+       
+       
+    @staticmethod
+    def get_all():
+        with get_cursor as cursor:
+            cursor.execute("SELECT * FROM owners")
+            return cursor.fetchall()
+        
+
+    @staticmethod
+    def get_by_id(owner_id):
+        with get_cursor() as cursor:
+            cursor.execute("SELECT * FROM owner WHERE id = ?", (owner_id,))
+            return cursor.fetchone()
+        
+
+
+class Tenant:
+    def __init__(self, name, contact_info, lease_start_date, lease_end_date, property_id):
+        self.name= name
+        self.contact_info= contact_info
+        self.lease_start_date= lease_start_date
+        self.lease_end_date= lease_end_date
+        self.property_id= property_id
+
+    
+    def save(self):
+        with get_cursor() as cursor:
+            cursor.execute("INSERT INTO tenants (name, contact_info, lease_start_date, lease_end_date, property_id VALUES (?, ?, ?, ?, ?)",
+                           (self.name, self.contact_info, self.lease_start_date, self.lease_end_date, self.property_id))
+            
+    
+
+    @staticmethod
+    def get_all():
+        with get_cursor() as cursor:
+            cursor.execute("SELECT * FROM tenants")
+            return cursor.fetchall()
+    
+
+    @staticmethod
+    def get_by_id(tenant_id):
+        with get_cursor() as cursor:
+            cursor.execute("SELECT * FROM tenants WHERE id=?", (tenant_id,))
+            return cursor.fetchone()
+
+
+
+    #Section for adding methods like delete, update etc
+
+
+class RentPayment:
+    def __init__ (self, tenant_id, property_id, payment_date, amount):
+        self.tenant_id = tenant_id
+        self.property_id= property_id
+        self.payment_date= payment_date
+        self.amount= amount
+
+
+    def save(self):
+        with get_cursor() as cursor:
+            cursor.execute("INSERT INTO rent_payments (tenant_id, property_id, payment_date, amount) VALUES(?, ?, ?, ? )", 
+                           (self.tenant_id, self.property_id, self.payment_date, self.amount))
+            
+
+
+    @staticmethod
+    def get_all():
+        with get_cursor() as cursor:
+            cursor.execute("SELECT * FROM rent_payments")
+            return cursor.fetchall()
+        
+
+    @staticmethod
+    def get_by_id(payment_id):
+        with get_cursor() as cursor:
+            cursor.execute("SELECT * FROM rent_payment WHERE id= ?", (payment_id,))
+            return cursor.fetchone()
+
+
+    # add methods (delete, update, etc)
+
+
+
+class MaintenanceRequest:
+    def __init__ (self, description, tenant_id, property_id, status="open"):
+        self.description = description
+        self.tenant_id= tenant_id
+        self.property_id= property_id
+        self.status= status
+
+
+    def save(self):
+        with get_cursor() as cursor:
+            cursor.execute("INSERT INTO maintenance_requests (description, tenant_id, property_id, status) VALUES (?, ?, ?, ?)", 
+                           (self.description, self.tenant_id, self.property_id, self.status))
+            
+    
+    @staticmethod
+    def get_all():
+        with get_cursor() as cursor:
+            cursor.execute("SELECT * FROM maintenance_requests")
+            return cursor.fetchall()
+        
+
+    @staticmethod
+    def get_by_id(request_id):
+        with get_cursor() as cursor:
+            cursor.execute("SELECT * FROM maintenance_requests WHERE id= ?", (request_id,))
+            return cursor.fetchone()
+        
+
+    # add other methods (delete, update etc)
+
+
+
+class Expense:
+    def __init__ (self, description, amount, date, property_id):
+        self.description = description
+        self.amount= amount
+        self.date= date
+        self.property_id= property_id
+
+
+    def save(self):
+        with get_cursor() as cursor:
+            cursor.execute("""
+INSERT INTO expenses (description, amount, date, property_id) VALUES(?, ?, ?, ?)
+""", (self.description, self.amount, self.date, self.property_id))
+            
+    
+    @staticmethod
+    def get_all():
+        with get_cursor() as cursor:
+            cursor.execute("SELECT * FROM expenses")
+            return cursor.fetchall()
+        
+
+    @staticmethod
+    def get_by_id(expense_id):
+        with get_cursor() as cursor:
+            cursor.execute("SELECT * FROM expenses WHERE id = ?", (expense_id,))
+            return cursor.fetchone()
+        
+
+
+    # add methods as needed (delete, update)
+
+
+class Document:
+    def __init__ (self, name, file_path, property_id, access_level):
+        self.name= name
+        self.file_path= file_path
+        self.property_id = property_id
+        self.access_level= access_level
+
+    
+
+    def save(self):
+        with get_cursor() as cursor:
+            cursor.execute("""
+INSERT INTO documents (name, file_path, property_id, access_level) VALUES (?, ?, ?, ?)
+""", (self.name, self.file_path, self.property_id, self.access_level))
+            
+
+    
+    @staticmethod
+    def get_all():
+        with get_cursor as cursor:
+            cursor.execute("SELECT * FROM documents ")
+            return cursor.fetchall()
+
+
+    
+    @staticmethod
+    def get_by_id(document_id):
+        with get_cursor() as cursor:
+            cursor.execute("SELECT * FROM documents WHERE id= ?", (document_id,))
+            return cursor.fetchone()
+        
+
+    #add other methods(delete, update)
+
+
